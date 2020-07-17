80564504bd4c02e84f9e508bbcf4a3d53cd59fb6
varenyamBakshi <varenyam101@gmail.com>
2020-07-17 22:06:20 +0530

    successfully implemented the training loop

--- value_iteration_on_frozenlake.py
+++ value_iteration_on_frozenlake.py
@@ -24,11 +24,11 @@ class Agent:
             self.state = self.env.reset() if is_done else new_state
 
     def calc_action_value(self, state, action):
-        target_counts = self.transit[(state,action)]
+        target_counts = self.transits[(state,action)]
         total = sum(target_counts.values())
         action_value = 0.0
         for tgt_state, count in target_counts.items():
-            reward = self.reward[(state,action,tgt_state)]
+            reward = self.rewards[(state,action,tgt_state)]
             action_value += (count/total)*(reward + gamma*self.values[tgt_state])
         return action_value
 
@@ -45,10 +45,10 @@ class Agent:
         total_reward = 0.0
         state = env.reset()
         while True:
-            action = slef.select_action(state)
+            action = self.select_action(state)
             new_state, reward, is_done, __ = env.step(action)
             self.rewards[(state,action,new_state)] = reward
-            self.transit[(state,action)][new_state] += 1
+            self.transits[(state,action)][new_state] += 1
             total_reward += reward
             if is_done: break
             state = new_state
@@ -56,7 +56,30 @@ class Agent:
 
     def value_iteration(self):
         for state in range(self.env.observation_space.n):
-            state_value = [self.calc_action_value(state,action) for action in range(self.env.action_space.n)]
+            state_values = [self.calc_action_value(state,action) for action in range(self.env.action_space.n)]
             self.values[state] = max(state_values)
 
-    
+if __name__ == "__main__":
+    test_env = gym.make(ENV_name)
+    agent = Agent()
+    writer = SummaryWriter("value_iteration_fl-v0")
+
+    iter_no = 0
+    best_reward = 0.0
+    while True:
+        iter_no += 1
+        agent.play_n_random_steps(100)
+        agent.value_iteration()
+        reward = 0.0
+        for _ in range(test_episodes):
+            reward += agent.play_episode(test_env)
+        
+        reward /= test_episodes
+        writer.add_scalar("reward", reward, iter_no)
+        if reward > best_reward:
+            print("Best reward updated %.3f -> %.3f"%(best_reward,reward))
+            best_reward = reward
+        if reward > 0.80:
+            print("Solved in %d iterations!"%(iter_no))
+            break
+        writer.close()
