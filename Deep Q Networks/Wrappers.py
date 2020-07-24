c508fb638c15895f2e076fc208937acc5f6ba9df
varenyamBakshi <varenyam101@gmail.com>
2020-07-24 15:51:52 +0530

    completed the wrappers.py withthe necessary wrapper classes created

--- Deep Q Networks/Wrappers.py
+++ Deep Q Networks/Wrappers.py
@@ -3,3 +3,121 @@ import gym
 import gym.spaces
 import numpy as np
 import collections
+
+#these are the classes taken from openai baselines repo
+# the link is https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
+
+class FireResetEnv(gym.Wrapper): # this presses the fire button to start the game
+    def __init__(self, env=None):
+        super(FireResetEnv, self).__init__(env)
+        assert env.unwrapped.get_action_meanings()[1]=="FIRE"
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def step(self,action):
+        return self.env.step.action()
+    
+    def reset(self):
+        self.env.reset()
+        obs, _, is_done, _ = self.env.step(1)
+        if is_done: self.env.reset()
+        obs, _, is_done, _ = self.env.step(2)
+        if is_done: self.env.reset()
+        return obs
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env=None, skip=4):
+        """Return only every 'skip'-th frame"""
+        super(MaxAndSkipEnv, self).__init__(env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = collections.deque(maxlen=2)
+        self._skip = skip
+    def step(self, action):
+        total_reward = 0.0
+        done = None
+        for _ in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            self._obs_buffer.append(obs)
+            total_reward += reward
+            if done: break
+        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+        return max_frame, total_reward, done, info
+    def _reset(self):
+        self._obs_buffer.clear()
+        obs = self.env.reset()
+        self._obs_buffer.append(obs)
+        return obs  
+
+class ProcessFrame84(gym.ObservationWrapper):
+    """This wrapper converts input observations from the emulator,
+    which normally has a resolution of 210 × 160 pixels with RGB color
+    channels, to a grayscale 84 × 84 image."""
+    def __init__(self, env=None):
+        super(ProcessFrame84, self).__init__(env)
+        self.observation_space = gym.spaces.Box(low=0,
+        high=255, shape=(84, 84, 1), dtype=np.uint8)
+    
+    def observation(self, obs):
+        return ProcessFrame84.process(obs)
+
+    @staticmethod
+    def process(frame):
+        if frame.size == 210 * 160 * 3:
+            img = np.reshape(frame, [210, 160,3]).astype(np.float32)
+        elif frame.size == 250 * 160 * 3:
+            img = np.reshape(frame, [250, 160,3]).astype(np.float32)
+        else:
+            assert False, "Unknown resolution."
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        resized_screen = cv2.resize(img, (84, 110),interpolation=cv2.INTER_AREA)
+        x_t = resized_screen[18:102, :]
+        x_t = np.reshape(x_t, [84, 84, 1])
+        return x_t.astype(np.uint8)
+
+
+class BufferWrapper(gym.ObservationWrapper):
+    """This class creates a stack of subsequent frames along the first dimension
+    and returns them as an observation. The purpose is to give the network an
+    idea about the dynamics of the objects"""
+    
+    def __init__(self, env, n_steps, dtype=np.float32):
+        super(BufferWrapper, self).__init__(env)
+        self.dtype = dtype
+        old_space = env.observation_space
+        self.observation_space = gym.spaces.Box(old_space.low.repeat(n_steps, axis=0),
+        old_space.high.repeat(n_steps, axis=0), dtype=dtype)
+    def reset(self):
+        self.buffer = np.zeros_like(self.observation_space.low,
+        dtype=self.dtype)
+        return self.observation(self.env.reset())
+    def observation(self, observation):
+        self.buffer[:-1] = self.buffer[1:]
+        self.buffer[-1] = observation
+        return self.buffer
+
+class ImageToPyTorch(gym.ObservationWrapper):
+    """This simple wrapper changes the shape of the observation from HWC to the
+    CHW format required by PyTorch. The input shape of the tensor has a color
+    channel as the last dimension, but PyTorch's convolution layers assume the
+    color channel to be the first dimension"""
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0.0,
+        high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]),dtype=np.float32)
+    def observation(self, observation):
+        return np.moveaxis(observation, 2, 0)
+
+class ScaledFloatFrame(gym.ObservationWrapper):
+    """The final wrapper we have in the library converts observation data from
+    bytes to floats and scales every pixel's value to the range [0.0...1.0]."""
+    def observation(self, obs):
+        return np.array(obs).astype(np.float32) / 255.0
+
+def make_env(env_name):
+    env = gym.make(env_name)
+    env = MaxAndSkipEnv(env)
+    env = FireResetEnv(env)
+    env = ProcessFrame84(env)
+    env = ImageToPyTorch(env)
+    env = BufferWrapper(env, 4)
+    return ScaledFloatFrame(env)
